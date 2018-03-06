ad64c6993e57903cc4ffe844989cf6b58da38401
Jonas Juffinger <mail@jonasjuffinger.com>
2018-03-06 14:26:08 +0100

    settings are saved and restored

--- app/src/main/java/com/jonasjuffinger/timelapse/SettingsActivity.java
+++ app/src/main/java/com/jonasjuffinger/timelapse/SettingsActivity.java
@@ -59,46 +59,30 @@ public class SettingsActivity extends BaseActivity
         bnClose = (Button) findViewById(R.id.bnClose);
         bnClose.setOnClickListener(bnCloseOnClickListener);
 
-        /*tabHost = (TabHost)findViewById(R.id.tabHost);
-        tabHost.setup();
-
-        //Tab 1
-        TabHost.TabSpec spec = tabHost.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("");
-        tabHost.addTab(spec);
-
-        //Tab 2
-        spec = tabHost.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("");
-        tabHost.addTab(spec);*/
-
         tvIntervalValue = (TextView) findViewById(R.id.tvIntervalValue);
         tvIntervalUnit = (TextView) findViewById(R.id.tvIntervalUnit);
-
-        sbInterval = (AdvancedSeekBar) findViewById(R.id.sbInterval);
-        sbInterval.setMax(83);
-        sbInterval.setOnSeekBarChangeListener(sbIntervalOnSeekBarChangeListener);
-        sbInterval.setProgress(settings.rawInterval);
-
-        tvShotsValue = (TextView) findViewById(R.id.tvShotsValue);
-
-        sbShots = (AdvancedSeekBar) findViewById(R.id.sbShots);
-        sbShots.setMax(130);
-        sbShots.setOnSeekBarChangeListener(sbShotsOnSeekBarChangeListener);
-        sbShots.setProgress(settings.rawShotCount);
 
         tvDurationValue = (TextView) findViewById(R.id.tvDurationValue);
         tvDurationUnit = (TextView) findViewById(R.id.tvDurationUnit);
         tvVideoTimeValue = (TextView) findViewById(R.id.tvVideoTimeValue);
         tvVideoTimeUnit = (TextView) findViewById(R.id.tvVideoTimeUnit);
-
+        sbInterval = (AdvancedSeekBar) findViewById(R.id.sbInterval);
+        tvShotsValue = (TextView) findViewById(R.id.tvShotsValue);
+        sbShots = (AdvancedSeekBar) findViewById(R.id.sbShots);
         spnFps = (Spinner) findViewById(R.id.spnFps);
+        cbSilentShutter = (CheckBox) findViewById(R.id.cbSilentShutter);
+
+        sbInterval.setMax(83);
+        sbInterval.setOnSeekBarChangeListener(sbIntervalOnSeekBarChangeListener);
+        sbInterval.setProgress(settings.rawInterval);
+
+        sbShots.setMax(130);
+        sbShots.setOnSeekBarChangeListener(sbShotsOnSeekBarChangeListener);
+        sbShots.setProgress(settings.rawShotCount);
+
         spnFps.setSelection(settings.fps);
         spnFps.setOnItemSelectedListener(spnFpsOnItemSelectedListener);
 
-        cbSilentShutter = (CheckBox) findViewById(R.id.cbSilentShutter);
         cbSilentShutter.setChecked(settings.silentShutter);
         cbSilentShutter.setOnCheckedChangeListener(cbSilentShutterOnCheckListener);
     }
@@ -115,6 +99,7 @@ public class SettingsActivity extends BaseActivity
     bnCloseOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            settings.save(that);
             finish();
         }
     };
@@ -125,6 +110,8 @@ public class SettingsActivity extends BaseActivity
         public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
             int intervalTextValue = 0;
             String intervalUnit = "";
+
+            settings.rawInterval = i;
 
             i++;
 
@@ -158,6 +145,8 @@ public class SettingsActivity extends BaseActivity
         @Override
         public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
             String shotsText;
+
+            settings.rawShotCount = i;
 
             i++;
 
@@ -199,6 +188,7 @@ public class SettingsActivity extends BaseActivity
         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
             String sfps = getResources().getStringArray(R.array.fps)[i];
             fps = Integer.parseInt(sfps);
+            settings.fps = i;
             updateTimes();
         }
 
